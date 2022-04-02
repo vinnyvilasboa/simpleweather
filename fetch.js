afcde289bc2d9a552cc62ee64eeaff7579602eb0
vinnyvilasboa <vinnycesca@gmail.com>
2022-04-02 16:36:21 -0400

    new layout

--- fetch.js
+++ fetch.js
@@ -1,6 +1,7 @@
 class Fetch {
     async getCurrent(input) {
       const myKey = "772a5938a64f5d0f8aa3e42401341db0";
+      
   
       //make request to url
   
@@ -15,3 +16,19 @@ class Fetch {
       return data;
     }
   }
+
+  /*
+  class Fetch {
+
+      async getCurrent(input){
+          const myKey = 'key####'
+
+          const response = await fetch (
+              `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${myKey}`
+          )
+          const data = await response.json()
+          console.log(data)
+          return data
+      }
+  }
+   */
