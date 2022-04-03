31060eeb680c8f6f234bd5554209f75b91fdec7a
vinnyvilasboa <vinnycesca@gmail.com>
2022-04-03 16:32:39 -0400

    imperial from kelvin conversion

--- fetch.js
+++ fetch.js
@@ -1,34 +1,35 @@
+//create new class function OOP
 class Fetch {
+    //
     async getCurrent(input) {
-      const myKey = "772a5938a64f5d0f8aa3e42401341db0";
-      
-  
-      //make request to url
-  
-      const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${myKey}`
-      );
-  
-      const data = await response.json();
-  
-      console.log(data);
-  
-      return data;
+        const myKey = "772a5938a64f5d0f8aa3e42401341db0";
+
+
+        //make request to url
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=imperial&appid=${myKey}`
+        );
+
+        const data = await response.json();
+
+        console.log(data);
+
+        return data;
     }
-  }
+}
 
-  /*
-  class Fetch {
+/*
+class Fetch {
 
-      async getCurrent(input){
-          const myKey = 'key####'
+    async getCurrent(input){
+        const myKey = 'key####'
 
-          const response = await fetch (
-              `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${myKey}`
-          )
-          const data = await response.json()
-          console.log(data)
-          return data
-      }
-  }
-   */
+        const response = await fetch (
+            `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${myKey}`
+        )
+        const data = await response.json()
+        console.log(data)
+        return data
+    }
+}
+ */
--- ui.js
+++ ui.js
@@ -2,7 +2,7 @@ class UI {
     constructor() {
       this.uiContainer = document.getElementById("content");
       this.city;
-      this.defaultCity = "London";
+      this.defaultCity = "New York";
     }
   
     populateUI(data) {
@@ -14,9 +14,9 @@ class UI {
           
           <div class="card mx-auto mt-5" style="width: 18rem;">
               <div class="card-body justify-content-center">
-                  <h5 class="card-title">${data.name}</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Highs of ${data.main.temp_max}. Lows of ${data.main.temp_min}</h6>
-                  <p class="card-text ">Weather conditions are described as: ${data.weather[0].description}</p>
+                  <h1 class="card-title" >${data.name}</h1>
+                  <h2 class="card-subtitle mb-2 text-muted">Highs of ${data.main.temp_max}. Lows of ${data.main.temp_min}</h2>
+                  <h3 class="card-text ">Weather conditions are described as: ${data.weather[0].description}</h3>
                   
               </div>
           </div>
