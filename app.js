f06fefc8510c0deebe7023280d60e5ea239b66c7
vinnyvilasboa <vinnycesca@gmail.com>
2022-04-05 13:59:47 -0400

    added current weather

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 //inst classes//
-/*this is the last one and everything will be called here */
-//realistically everything could've been put here but for the sake of keeping things clean it was split up.
 const ft = new Fetch();
 const ui = new UI();
 
 //add event listeners//
-
 const search = document.getElementById("searchUser");
 const button = document.getElementById("submit");
+
 button.addEventListener("click", () => {
-  const currentVal = search.value;
+
+  const currentVal = search.value
 
   ft.getCurrent(currentVal).then((data) => {
     //call a UI method//
