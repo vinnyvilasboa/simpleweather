e5c0d203c9b6634bfdaac839919bd868a2a6a1ae
vinnyvilasboa <vinnycesca@gmail.com>
2022-04-07 12:47:40 -0400

    rounded down

--- tempCodeRunnerFile.js
+++
@@ -1,2 +0,0 @@
-
-        const data = await response.js
--- ui.js
+++ ui.js
@@ -1,22 +1,25 @@
 class UI {
     constructor() {
-      this.uiContainer = document.getElementById("content");
-      this.city;
-      this.defaultCity = "New York";
+        this.uiContainer = document.getElementById("content");
+        this.city;
+        this.defaultCity = "New York";
     }
-  
+
     populateUI(data) {
-      //de-structure vars
-  
-      //add them to inner HTML
-  
-      this.uiContainer.innerHTML = `
+        //de-structure vars
+
+        //add them to inner HTML
+        let current = Math.floor(data.main.temp)
+        let high = Math.floor(data.main.temp_max)
+        let low = Math.floor(data.main.temp_min)
+        this.uiContainer.innerHTML = `
           
           <div class="card mx-auto mt-5" style="width: 18rem;"> 
               <div class="card-body justify-content-center">
                   <h1 class="card-title" >${data.name}</h1>
-                  <h2 class="card-subtitle mb-2 text-muted">› Currently: ${data.main.temp}</h2>
-                  <h2 class="card-subtitle mb-2 text-muted">› Highs of ${data.main.temp_max} Lows of ${data.main.temp_min}</h2>
+                  <h2 class="card-subtitle mb-2 text-muted">› Currently: ${current}</h2>
+                  <h2 class="card-subtitle mb-2 text-muted">› Highs of ${high}</h2>
+                  <h2 class="card-subtitle mb-2 text-muted">› Lows of ${low}</h2>
                   <h3 class="card-text "><i>Weather conditions:</i> </br> ${data.weather[0].description}</h3>
                   
               </div>
@@ -25,29 +28,28 @@ class UI {
           
           `;
     }
-  
+
     clearUI() {
-      uiContainer.innerHTML = "";
+        uiContainer.innerHTML = "";
     }
-  
+
     saveToLS(data) {
-      localStorage.setItem("city", JSON.stringify(data));
+        localStorage.setItem("city", JSON.stringify(data));
     }
-  
+
     getFromLS() {
-      if (localStorage.getItem("city" == null)) {
-        return this.defaultCity;
-      } else {
-        this.city = JSON.parse(localStorage.getItem("city"));
-      }
-  
-      return this.city;
+        if (localStorage.getItem("city" == null)) {
+            return this.defaultCity;
+        } else {
+            this.city = JSON.parse(localStorage.getItem("city"));
+        }
+
+        return this.city;
     }
-  
+
     clearLS() {
-      localStorage.clear();
+        localStorage.clear();
     }
-  }
+}
 
 
- 
